96a09f82b2584ddca6201f62ddec6ef8f3f195a2
KomoreKalu <komorekalu@outlook.com>
2020-07-29 11:53:25 +0800

    add uninstall.sh

---
+++ tool/uninstall.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+reset_font(){
+	gsettings reset org.gnome.desktop.interface font-name 
+	gsettings reset org.gnome.desktop.interface document-font-name 
+	gsettings reset org.gnome.desktop.interface monospace-font-name 
+	gsettings reset org.gnome.desktop.wm.preferences titlebar-font
+	sudo rm -rv /usr/share/fonts/NotoSansCJKsc /usr/share/fonts/NotoSerifCJKsc /usr/share/fonts/JetBrainsMono /etc/fonts/conf.d/64-language-selector-prefer.conf
+	fc-cache -fv
+}
+
+reset_theme(){
+	gsettings reset org.gnome.desktop.interface gtk-theme 
+	sudo apt purge arc-theme -y
+}
+
+reset_icon(){
+	gsettings reset org.gnome.desktop.interface icon-theme
+	sudo apt purge tela* -y
+}
+
+main(){
+	reset_font
+	reset_theme
+	reset_icon
+}
+
+main	
