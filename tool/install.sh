93f3abbb18e4b98066ab33abf6c11d5194eb80d1
KomoreKalu <34343603+KomoreKalu@users.noreply.github.com>
2020-12-11 23:55:32 +0800

    Update install.sh

--- tool/install.sh
+++ tool/install.sh
@@ -34,7 +34,6 @@ setup_icon(){
 
 other(){
 	gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:minimize,maximize,close'
-	#gsettings set org.gnome.shell.calendar show-weekdate true
 }
 
 main(){
