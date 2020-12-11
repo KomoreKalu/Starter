91f53bbb5fc8dcb438254e45a4e079259d68b070
KomoreKalu <34343603+KomoreKalu@users.noreply.github.com>
2020-12-11 23:53:59 +0800

    Update install.sh

--- tool/install.sh
+++ tool/install.sh
@@ -11,7 +11,7 @@ setup_font(){
 	cd NotoSansCJKsc && sudo mkfontdir && sudo mkfontscale && cd ..
 	cd NotoSerifCJKsc && sudo mkfontdir && sudo mkfontscale && cd ..
 	cd JetBrainsMono && sudo mkfontdir && sudo mkfontscale && cd ..
-	wget -c https://raw.githubusercontent.com/KomoreKalu/starter/master/config/font.conf -O - | sudo cat - > /etc/fonts/conf.d/64-language-selector-prefer.conf
+	sudo sh -c 'wget -c https://raw.githubusercontent.com/KomoreKalu/starter/master/config/font.conf -O - | cat - > /etc/fonts/conf.d/64-language-selector-prefer.conf'
 	fc-cache -fv
 	gsettings set org.gnome.desktop.interface font-name 'Noto Sans CJK SC 13'
 	gsettings set org.gnome.desktop.interface document-font-name 'Noto Sans CJK SC 13'
